17d962fd9c6d8c0b81e0382947862ee7036eda18
ppennyy <145088338+ppennyy@users.noreply.github.com>
2024-10-16 11:48:35 +0800

    Add files via upload

---
+++ week06/week06-2a.cpp
@@ -0,0 +1,15 @@
+///week06-2a.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int>a;
+    int now;
+    while(cin>>now){
+        a.push_back(now);
+        cout<<now<<"\n";
+    }
+}
+
