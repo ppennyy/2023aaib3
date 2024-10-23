48c122006b8a771d3164bc1cfceb63fa87208618
ppennyy <145088338+ppennyy@users.noreply.github.com>
2024-10-23 11:50:06 +0800

    Add files via upload

---
+++ week07/week07-3.cpp
@@ -0,0 +1,17 @@
+//week07-3.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	vector<int> a;
+	int now;
+	while(cin>>now){
+		a.push_back(now);
+		sort(a.begin(),a.end());
+		int N=a.size();
+		if(N%2==1)cout<<a[N/2]<<"\n";
+		else cout <<(a[N/2-1]+a[N/2])/2<<"\n";
+	}
+}
