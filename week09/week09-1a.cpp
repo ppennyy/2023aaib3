03b3bc4f9e55560286b1adadf27e121cde6dd8f3
ppennyy <145088338+ppennyy@users.noreply.github.com>
2024-11-06 11:59:26 +0800

    Add files via upload

---
+++ week09/week09-1a.cpp
@@ -0,0 +1,21 @@
+//week09-1a.cpp
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int M = matrix.size();
+        int N = matrix[0].size();
+        int i = 0,j = 0, dir = 0;
+        int dI[4] = {0,1,0,-1};
+        int dJ[4] = {1,0,-1,0};
+
+        vector<int> ans;
+        ans.push_back(matrix[i][j]);
+        i+=dI[dir];
+        j+=dJ[dir];
+        ans.push_back(matrix[i][j]);
+        i+=dI[dir];
+        j+=dJ[dir];
+        ans.push_back(matrix[i][j]);
+        return ans;
+    }
+};
