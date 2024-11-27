77e89449220bc6fa7033d5788692486b0653a8b4
ppennyy <145088338+ppennyy@users.noreply.github.com>
2024-11-27 11:57:19 +0800

    Add files via upload

---
+++ week12/week12-1.cpp
@@ -0,0 +1,20 @@
+///week12-1.cpp
+#include<iostream>
+#include <string>
+using namespace std;
+///struct Node {};
+///class Cal {};
+struct Node {
+    int val;
+};
+class Cat{
+public:
+    string name;
+};
+int main()
+{
+    Cat cat1;
+    cat1.name="cattie";
+    Node node;
+    node.val=1;
+}
