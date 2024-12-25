c7787a1f02411ec2b33acd7fc972f14ea96bda8a
ppennyy <145088338+ppennyy@users.noreply.github.com>
2024-12-25 12:01:23 +0800

    Add files via upload

---
+++ week16/week16-2.cpp
@@ -0,0 +1,18 @@
+//week16-2.cpp
+class Solution {
+public:
+    double myPow(double x, long long int n) {
+        if(n==0)return 1;
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        if(n%2==0){
+            double now =myPow(x,n/2);
+            return now*now;
+        }else{
+            double now=myPow(x,n/2);
+            return now*now*x;
+        }
+    }
+};
